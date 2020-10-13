a702fb5d7314399210a3ba63b45ceadaa27a261e
Андрей Солонников <andreisolo.lite@gmail.com>
2020-10-13 11:27:59 +0300

    Changes

--- Flask_App/app.py
+++ Flask_App/app.py
@@ -1,5 +1,16 @@
 from flask import Flask, render_template, request, json
+from flaskext.mysql import MySQL
+import config
+
 app = Flask(__name__)
+mysql = MySQL()
+
+
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = config.password_db
+app.config['MYSQL_DATABASE_DB'] = 'BucketList'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
 
 
 @app.route('/')
@@ -10,21 +21,36 @@ def main():
 def showSignUp():
 	return render_template('signup.html')
 
-@app.route('/signUp', methods=['POST'])
+@app.route('/signUp', methods=['POST', 'GET'])
 def signUp():
+	try:
+		_name = request.form['inputName']
+		_email = request.form['inputEmail']
+		_password = request.form['inputPassword']
 
-	_name = request.form['inputName']
-	_email = request.form['inputEmail']
-	_password = request.form['inputPassword']
+		if _name and _email and _password:
+			conn = mysql.connect()
+			cursor = conn.cursor()
+			cursor.callproc('sp_createUser', (_name, _email, _password))
+			data = cursor.fetchall()
 
-	if _name and _email and _password:
-		return json.dumps({'html' : '<span>All fields good !!!</span>'})
-	else:
-		return json.dumps({'html' : '<span>Enter the required fields</span>'})
+			if len(data) is 0:
+				conn.commit()
+				return json.dumps({'message' : 'User created successfully !'})
+			else:
+				return json.dumps({'error' : str(data[0])})
+
+		else:
+			return json.dumps({'html' : '<span>Enter the required fields </span>'})
 
 
+	except Exception as e:
+		return json.dumps({'error' : str(e)})
+	finally:
+		cursor.close()
+		conn.close()
 
 if __name__ == '__main__':
-	app.run()
+	app.run(port=5000)
 
 
